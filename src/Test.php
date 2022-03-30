391eda8276ee87f425a96bb2659c3e35dd320506
codyht <codyht@163.com>
2022-03-30 11:48:07 +0800

    Initial commit

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小蛮哼哼哼
+ * Email: 243194993@qq.com
+ * Date: 2022/3/30
+ * Time: 11:46
+ * motto: 现在的努力是为了小时候吹过的牛逼！
+ */
+
+namespace Htlove;
+
+class Test
+{
+    public function text(){
+        echo time();
+    }
+}
